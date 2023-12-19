c0d3b5110dac510fcf31f4c5ae6c9f20eb93b1a5
08ppty <pemmarajupavan08@gmail.com>
2023-12-19 15:28:52 +0530

    Committing the changes

---
+++ src/test/java/restassuredscripts/Script010_LoggingMethods.java
@@ -0,0 +1,30 @@
+package restassuredscripts;
+
+
+
+import static io.restassured.RestAssured.*;
+
+import org.testng.annotations.Test;
+
+public class Script010_LoggingMethods {
+	
+	
+	@Test
+	public void LoggingRestAssured() {
+		
+		String PMapikey = "PMAK-use your key here";
+		
+		given().baseUri("https://api.postman.com")
+		.basePath("/workspaces")
+		.header("x-api-key", PMapikey)
+		.when().get()
+		.then().log().all();
+		//.then().log().headers();
+		//.then().log().ifError();
+		//.then().statusCode(200).log().cookies();
+		
+		
+	}
+
+}
+
---
+++ src/test/java/restassuredscripts/Script01SetUpCheck.java
@@ -0,0 +1,31 @@
+package restassuredscripts;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Script01SetUpCheck {
+
+	
+	
+	
+	
+	@Test
+	  public void setUpCheck() {
+		  
+		  
+ 
+			Response res = RestAssured.get("https://reqres.in/api/users/2");
+
+			System.out.println(res.asString());
+
+			System.out.println(res.asPrettyString()); // Convert the response into the string
+
+			System.out.println(res.getStatusCode());
+
+		}
+
+		  
+	  
+}
